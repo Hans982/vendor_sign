2ac1e8a3fe37c6d94db4c6f034b9f3d6b60b5ffc
Iverz <140925089+asterixiverz@users.noreply.github.com>
2025-02-03 16:25:23 +0700

    Adapt to LineageOS general keys

--- keys.mk
+++ keys.mk
@@ -1,2 +1,2 @@
 $(warning "Your build will be signed by Iverz key.")
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/orion-priv/keys/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
